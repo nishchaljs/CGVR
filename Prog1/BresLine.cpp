719c8a2bae534c606308f435d1db17357ca425e8
Nishchal J <nishchaljs@gmail.com>
2022-03-15 00:53:44 +0530

    Update BresLine.cpp

--- Prog1/BresLine.cpp
+++ Prog1/BresLine.cpp
@@ -1,109 +1,127 @@
-#include<stdio.h>
+//bres_line - prog1
 #include<gl/glut.h>
-int flag = 0;
-int x1, x2, y1, y2;
-
-void bresdrawpixel(int x, int y) {
-	printf("Draw Pixel was called\n");
-	glColor3f(1.0, 0.0, 0.0);
-	glPointSize(5.0);
-	glBegin(GL_POINTS);
-	glVertex2i(x, y);
-	glEnd();
-	glFlush();
-}
-void drawbresline(int x1, int x2, int y1, int y2) {
-	int dx = x2 - x1, dy = y2 - y1;
+#include<stdio.h>
+int x1=0, x2=0, y1=0, y2=0, choice=0, click=0;
+void bres(int x1, int y1, int x2, int y2) {
+	int dx = x2 - x1, dy = y2 - y1, i=0, incx=1, incy=1;
 	if (dx == 0) {
-		for (int i = 0; i <= dy; i++)
-			bresdrawpixel(x1, y1 + i);
-			return ;
+		glBegin(GL_POINTS);
+		for (i = 0; i <= (y2 - y1); i++) {
+			glVertex2d(x1, y1 + i);
+		}
+		glEnd();
+		glFlush();
 	}
-
 	if (dy == 0) {
-		for (int i = 0; i <= dx; i++)
-			bresdrawpixel(x1+i, y1);
-		return;
+		glBegin(GL_POINTS);
+		for (i = 0; i <= (x2 - x1); i++) {
+			glVertex2d(x1+i, y1);
+		}
+		glEnd();
+		glFlush();
 	}
-	int inx = 1, iny = 1;
-	if (dx < 0)
+	if (dx < 0) {
+		incx = -1;
 		dx = -dx;
-	if(x2<x1)
-		inx=-1;
-	if (dy < 0)
+	}
+	if (dy < 0) {
+		incy = -1;
 		dy = -dy;
-	if (y2 < y1)
-		iny = -1;
-
-	if (dx > dy) {
+	}
+	if (dy < dx) {
 		int e = 2 * dy - dx;
-		int inc1 = 2 * (dy - dx);
-		int inc2 = 2 * dy;
-		int x=x1, y=y1;
-		for (int i = 0; i <= dx; i++) {
-			if (e > 0) {
-				e += inc1;
-				y += iny;
-			}
-			else {
-				e += inc2;
-			}
-			x += inx;
-			bresdrawpixel(x, y);
-		}
-	}
-
-	else{
-		int e = 2 * dx - dy;
-		int inc1 = 2 * (dx - dy);
-		int inc2 = 2 * dx;
+		int inc1 = 2 * dy;
+		int inc2 = 2 * (dy - dx);
 		int x = x1, y = y1;
-		for (int i = 0; i <= dy; i++) {
-			if (e > 0) {
+		glBegin(GL_POINTS);
+		for (i = 0; i <= dx; i++) {
+			glVertex2d(x, y);
+			if (e < 0) {
 				e += inc1;
-				x += inx;
 			}
 			else {
 				e += inc2;
+				y+=incy;
 			}
-			y += iny;
-			bresdrawpixel(x, y);
+			x+=incx;
 		}
+		glEnd();
+		glFlush();
 	}
 
-	glFlush();
-}
-void bresDisplay(){
-	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(1.0,1.0,0.0,0.0);
-	gluOrtho2D(0, 500, 0, 500);
-	//glColor3f(0, 0, 1);
-	glFlush();
-}
-void bresMouse(int button, int state, int x, int y) {
-	if(button==GLUT_LEFT_BUTTON && state == GLUT_DOWN && flag==0){
-		x1 = x;
-		y1 = 500 - y ;
-		printf("X1=%d, Y1=%d\n", x1, y1);
-		flag = 1;
+	else {
+		int e = 2 * dx - dy;
+		int inc1 = 2 * dx;
+		int inc2 = 2 * (dx - dy);
+		int x = x1, y = y1;
+		glBegin(GL_POINTS);
+		for (i = 0; i <= dy; i++) {
+			glVertex2d(x, y);
+			if (e < 0) {
+				e += inc1;
+			}
+			else {
+				e += inc2;
+				x+=incx;
+			}
+			y+=incy;
+		}
+		glEnd();
+		glFlush();
 	}
-	else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && flag == 1) {
+
+}
+
+void mouse(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && click==0) {
+		x1 = x;
+		y1 = 500 - y;
+		click = 1;
+		printf("%d %d\n", x1, y1);
+	}
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && click == 1) {
 		x2 = x;
 		y2 = 500 - y;
-		flag = 0;
-		printf("X2=%d, Y2=%d\n", x2, y2);
-		drawbresline(x1, x2, y1, y2);
+		click = 0;
+		printf("%d %d\n", x2, y2);
+		bres(x1, y1, x2, y2);
 	}
+
 }
-int main(int argc, char** argv) {
+
+void display() {
+	glClearColor(1, 1, 1, 1);
+	bres(x1, y1, x2, y2);
+	glFlush();
+	glutPostRedisplay();
+}
+
+void display1() {
+	glClearColor(1, 1, 1, 1);
+	glFlush();
+	glutPostRedisplay();
+}
+
+void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(500, 500);
-	glutCreateWindow("Bres Line");
-	glutDisplayFunc(bresDisplay);
-	glutMouseFunc(bresMouse);
-	//bresinit();
-	glutMainLoop();
-	return 0;
+	glutCreateWindow("Bres_line");
+	glOrtho(0, 500, 0, 500, 0, 500);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	printf("Enter 1 for keyboard input, 2 for mouse input:\n");
+	scanf_s("%d", &choice);
+	if (choice == 1) {
+		printf("Enter coordinates X1 Y1 X2 Y2\n");
+		scanf_s("%d%d%d%d", &x1, &y1, &x2, &y2);
+		glutDisplayFunc(display);
+		glutMainLoop();
+	}
+	else {
+		glutDisplayFunc(display1);
+		glutMouseFunc(mouse);
+		glutMainLoop();
+	}
 }
