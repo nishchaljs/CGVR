330d673c573d81c559c2656cf83b9da762a4a350
Nishchal J <nishchaljs@gmail.com>
2022-03-15 00:54:40 +0530

    Update Circle.cpp

--- Prog2/Circle.cpp
+++ Prog2/Circle.cpp
@@ -1,68 +1,96 @@
+//BresCircle
 #include<stdio.h>
-#include<math.h>
 #include<gl/glut.h>
-int cx, cy, rx, ry,cflag=0;
-void circledisplay() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glPointSize(5.0);
+#include<math.h>
+int xc=0, yc=0, r=0, choice=0, click=0;
+void plot(int h, int k, int x, int y) {
+	glColor3d(1, 0, 0);
+	glBegin(GL_POINTS);
+	glVertex2d(x+h, y+k);
+	glVertex2d(-x+h, y+k);
+	glVertex2d(x +h, -y+k);
+	glVertex2d(-x+h, -y+k);
+	glVertex2d(y+h, x+k);
+	glVertex2d(-y+h, x+k);
+	glVertex2d(y+h, -x+k);
+	glVertex2d(-y+h, -x+k);
+	glEnd();
 	glFlush();
 }
-void plotcirclepixel(int h, int k, int x, int y) {
-	printf("I am printing pixels\n");
-	glColor3f(0.0, 0.0, 1.0);
-	glBegin(GL_POINTS);
-	glVertex2f(x + h, y + k);
-	glVertex2f(x + h, -y + k);
-	glVertex2f(-x + h, y + k);
-	glVertex2f(-x + h, -y + k);
-	glVertex2f(y + h, x + k);
-	glVertex2f(-y + h, x + k);
-	glVertex2f(y + h, -x + k);
-	glVertex2f(-y + h, -x + k);
-	glEnd();
-}
-void drawcircle(int r) {
-	int x1 = 0, y1 = r, d = 1 - r;
-	while (y1 > x1) {
-		plotcirclepixel(cx, cy, x1, y1);
-		if (d < 0) {
-			d += 2 * x1 + 3;
+void bres(int h, int k, int r) {
+	int y = r, x = 0, e = 1 - r;
+	while (y > x) {
+		plot(h, k, x, y);
+		if (e < 0) {
+			e += 2 * x + 3;
 		}
 		else {
-			d += 2 * (x1 - y1) + 5;
-			y1--;
+			e += 2 * (x-y) + 5;
+			y--;
 		}
-		x1++;
+		x++;
 	}
-	plotcirclepixel(cx, cy, x1, y1);
+}
+void reshape(int w, int h) {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, 500, 0, 500, 0, 500);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glutPostRedisplay();
+}
+void mouse(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && click==0) {
+		xc = x;
+		yc = 500 - y;
+		click = 1;
+	}
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && click == 1) {
+		x = x;
+		y = 500 - y;
+		int r = sqrt((x - xc) * (x - xc) + (y - yc) * (y - yc));
+		bres(xc, yc, r);
+		click = 0;
+	}
+	glutPostRedisplay();
+}
+void display() {
+	glClearColor(1, 1, 1, 1);
+	bres(xc, yc, r);
 	glFlush();
+	glutPostRedisplay();
 }
-void circlemouse(int button, int state, int x, int y) {
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && cflag==0) {
-		cx = x;
-		cy = 500-y;
-		printf("Circle Center: %d %d\n", cx, cy);
-		cflag = 1;
-	}
-	else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && cflag == 1) {
-		rx = x;
-		ry = 500 - y;
-		cflag = 0;
-		int r = sqrt((cx - rx) * (cx - rx) + (cy - ry) * (cy - ry));
-		printf("Circle radius: %d\n", r);
-		drawcircle(r);
-	}
+
+void display2() {
+	glClearColor(1, 1, 1, 1);
+	glFlush();
+	glutPostRedisplay();
 }
-int main(int argc, char** argv) {
+
+
+void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(500, 500);
-	glutCreateWindow("Circle");
-	glutDisplayFunc(circledisplay);
-	glutMouseFunc(circlemouse);
-	gluOrtho2D(0, 500, 0, 500);
-	glutMainLoop();
-	return 0;
+	glutCreateWindow("Bres Circle");
+	glOrtho(0,500,0,500,0,500);
+	glClear(GL_COLOR_BUFFER_BIT);
+	printf("Enter 1 for Keyboard, 2 for mouse\n");
+	scanf_s("%d", &choice);
+	if (choice == 1) {
+		printf("Enter Centre X Y and Radius R\n");
+		scanf_s("%d%d%d", &xc, &yc, &r);
+		glutDisplayFunc(display);
+		glutReshapeFunc(reshape);
+		glutMainLoop();
+	}
+	else {
+		
+		glutDisplayFunc(display2);
+		glutMouseFunc(mouse);
+		glutReshapeFunc(reshape);
+		glutMainLoop();
+	}
+
 }
