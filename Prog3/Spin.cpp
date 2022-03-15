c735e7e9bd93fb6413b128052ebd575c859711bc
Nishchal J <nishchaljs@gmail.com>
2022-03-15 17:39:06 +0530

    Update Spin.cpp

--- Prog3/Spin.cpp
+++ Prog3/Spin.cpp
@@ -1,93 +1,108 @@
 #include<stdio.h>
 #include<gl/glut.h>
-float spinangle_s = 2, spinangle_d = 0.02;
-int t,id1,id2;
-void delayspin(int t) {
-	for (t; t > 0; t--)
-		for (int i = 0; i < 100000000; i++);
-}
-void spindoubledisplay() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_MATRIX_MODE);
-	glLoadIdentity();
-	glRotatef(spinangle_d, 0, 0, 1);
+float alpha = 0.0, beta=0.0, click2=0;
+void draw_rect() {
 	glBegin(GL_QUADS);
-	glVertex2f(0, 100);
-	glVertex2f(100, 0);
-	glVertex2f(0, -100);
-	glVertex2f(-100, 0);
+	glColor3d(1, 0, 0);
+	glVertex2d(200, 200);
+
+	glColor3d(0, 1, 0);
+	glVertex2d(300, 200);
+
+	glColor3d(0, 0, 1);
+	glVertex2d(300, 300);
+
+	glColor3d(1, 0, 1);
+	glVertex2d(200, 300);
 	glEnd();
+	glFlush();
+}
+void display2() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(1, 1, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(250, 250, 0);
+	glRotatef(beta, 0, 0, 1);
+	glTranslatef(-250, -250, 0);
+	draw_rect();
+	glPopMatrix();
+	glFlush();
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+void display1() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(1, 1, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(250, 250, 0);
+	glRotatef(alpha, 0, 0, 1);
+	glTranslatef(-250, -250, 0);
+	draw_rect();
 	glutSwapBuffers();
 	glFlush();
-	
-}
-void spinsingledisplay() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glRotatef(spinangle_s, 0, 0, 1);
-	glBegin(GL_QUADS);
-	glVertex2f(0, 100);
-	glVertex2f(100, 0);
-	glVertex2f(0, -100);
-	glVertex2f(-100, 0);
-	glEnd();
-	glFlush();
-}
-void idlespin() {
-	delayspin(1);
-	spinangle_d += 1;
-	if (spinangle_d > 360)
-		spinangle_d -= 360;
-	glutSetWindow(id2);
 	glutPostRedisplay();
 }
-void spinmousedouble(int button, int state, int x, int y) {
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-		glutIdleFunc(idlespin);
+void mouse1(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		alpha += 2;
 	}
-	else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-		glutIdleFunc(NULL);
+	glutPostRedisplay();
 }
-void spinmousesingle(int button, int state, int x, int y) {
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-		spinangle_s += 2;
-		if (spinangle_s > 360)
-			spinangle_s -= 360;
-		glutSetWindow(id1);
+void mouse2(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if (click2 == 0)
+			click2 = 1;
+		else
+			click2 = 0;
+	}
+	glutPostRedisplay();
+}
+void idle1() {
+	
+		glutPostRedisplay();
+
+}
+void idle2() {
+	if (click2 == 1) {
+		beta += 2;
 		glutPostRedisplay();
 	}
+	else {
+		//
+		glutPostRedisplay();
+	}
+	
 }
-
-void spinreshape(int w, int h) {
-	glViewport(0, 0, w, h);
+void reshape(int w, int h) {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(-w / 2, w / 2, -h / 2, h / 2);
+	glOrtho(0, 500, 0, 500, 0, 500);
 	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 	glutPostRedisplay();
 }
-
-int main(int argc, char** argv) {
+void main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(500, 500);
-
-	id1 = glutCreateWindow("Spin Single");
-	glutDisplayFunc(spinsingledisplay);
-	glutMouseFunc(spinmousesingle);
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glColor3f(1.0, 0.0, 0.0);
-	gluOrtho2D(-250, 250, -250, 250);
-
-	id2 = glutCreateWindow("Spin DOuble");
-	glutDisplayFunc(spindoubledisplay);
-	glutMouseFunc(spinmousedouble);
-	t = id1;
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glColor3f(1.0, 0.0, 0.0);
-	gluOrtho2D(-250, 250, -250, 250);
+	glutInitWindowPosition(0, 0);
+	glOrtho(0, 500, 0, 500, 0, 500);
 	
-	glutReshapeFunc(spinreshape);
+	int w1 = glutCreateWindow("Still");
+	glutDisplayFunc(display1);
+	glutMouseFunc(mouse1);
+	glutIdleFunc(idle1);
+	glutReshapeFunc(reshape);
+
+
+	int w2 = glutCreateWindow("Spin");
+	glutDisplayFunc(display2);
+	glutMouseFunc(mouse2);
+	glutIdleFunc(idle2);
+	glutReshapeFunc(reshape);
+
 	glutMainLoop();
-	return 0;
 }
