b4493044e01591bc53a73de75c44bf7701fdec0f
Nishchal J <nishchaljs@gmail.com>
2022-03-17 19:13:05 +0530

    Update Cube.cpp

--- Prog12/Cube.cpp
+++ Prog12/Cube.cpp
@@ -1,132 +1,127 @@
 #include<stdio.h>
 #include<gl/glut.h>
-#include<gl/glut.h>
+int cube[8][3] = { {1,1,1},{1,1,-1},{-1,1,-1},{-1,1,1},{-1,-1,1},{1,-1,1},{1,-1,-1},{-1,-1,-1} };
+int axes=0; float alpha = 2;
 
-GLfloat theta[] = { 0,0,0 };
-int axis = 1;
-void delay(int x) {
-	for (x; x > 0; x--)
-		for (int i = 0; i < 100000000; i++);
-}
-void cubedisplay() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity();
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glTranslatef(150, 150, -50);
-	if (axis == 0) {
-		glRotatef(theta[axis], 1, 0, 0);
-	}
-	if (axis == 1) {
-		glRotatef(theta[axis], 0, 1, 0);
-	}
-	if (axis == 2) {
-		glRotatef(theta[axis], 0, 0, 1);
-	}
-	glTranslatef(-150, -150, 50);
-
+void draw_cube() {
 
 	glBegin(GL_QUADS);
-	//1st face
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(100, 100, 0);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(100, 200, 0);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(200, 200, 0);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(200, 100, 0);
+	glColor3d(1, 0, 0);
+	glVertex3iv(cube[0]);
+	glColor3d(0, 1, 0);
+	glVertex3iv(cube[1]);
+	glColor3d(0, 0, 1);
+	glVertex3iv(cube[2]);
+	glColor3d(1, 0, 1);
+	glVertex3iv(cube[3]);
 
-	//2nd face
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(100, 100, 0);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(100, 200, 0);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(100, 200,-100);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(100, 100, -100);
+	glColor3d(1, 0, 0);
+	glVertex3iv(cube[4]);
+	glColor3d(0, 1, 0);
+	glVertex3iv(cube[5]);
+	glColor3d(0, 0, 1);
+	glVertex3iv(cube[6]);
+	glColor3d(1, 0, 1);
+	glVertex3iv(cube[7]);
+	
+	glColor3d(1, 0, 0);
+	glVertex3iv(cube[0]);
+	glColor3d(0, 1, 0);
+	glVertex3iv(cube[1]);
+	glColor3d(0, 0, 1);
+	glVertex3iv(cube[6]);
+	glColor3d(1, 0, 1);
+	glVertex3iv(cube[5]);
 
-	//3rd face
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(100, 100, -100);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(100, 200, -100);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(200, 200, -100);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(200, 100, -100);
+	glColor3d(1, 0, 0);
+	glVertex3iv(cube[0]);
+	glColor3d(0, 1, 0);
+	glVertex3iv(cube[5]);
+	glColor3d(0, 0, 1);
+	glVertex3iv(cube[4]);
+	glColor3d(1, 0, 1);
+	glVertex3iv(cube[3]);
+	
+	glColor3d(1, 0, 0);
+	glVertex3iv(cube[3]);
+	glColor3d(0, 1, 0);
+	glVertex3iv(cube[4]);
+	glColor3d(0, 0, 1);
+	glVertex3iv(cube[7]);
+	glColor3d(1, 0, 1);
+	glVertex3iv(cube[2]);
 
-	//4th face
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(200, 200, -100);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(200, 100, -100);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(200, 100, 0);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(200, 200, 0);
-
-	//5th face
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(200, 200, 0);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(200, 200, -100);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(100, 200, -100);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(100, 200, 0);
-
-	//6th face
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(100, 100, 0);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(100, 100, -100);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(200, 100, -100);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(200, 100, 0);
+	glColor3d(1, 0, 0);
+	glVertex3iv(cube[2]);
+	glColor3d(0, 1, 0);
+	glVertex3iv(cube[1]);
+	glColor3d(0, 0, 1);
+	glVertex3iv(cube[6]);
+	glColor3d(1, 0, 1);
+	glVertex3iv(cube[7]);
 	glEnd();
-
 	glFlush();
-}
 
-void idlecube() {
-	delay(2);
-	theta[axis] = 5;
-	glutPostRedisplay();
 }
-
-void cubemouse(int button, int state, int x, int y) {
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) axis = 0;
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) axis = 1;
-	if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) axis = 2;
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1, 1, 1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	if(axes==0)
+		glRotatef(alpha, 1,0,0);
+	else if (axes == 1)
+		glRotatef(alpha, 0, 1, 0);
+	if (axes == 2)
+		glRotatef(alpha, 0, 0, 1);
+	draw_cube();
+	glFlush();
+	glutPostRedisplay();	
 }
-
-void cubereshape(int w, int h) {
+void mouse(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		axes = 0;
+		glutPostRedisplay();
+	}
+	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+		axes = 1;
+		glutPostRedisplay();
+	}
+	else if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) {
+		axes = 2;
+		glutPostRedisplay();
+	}
+	
+}
+void reshape(int w, int h) {
 	glViewport(0,0,w,h);
+	printf("%d %d\n", w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	if (w <= h)
-		glOrtho(-2, 2 * (float)h / (float)w, -2, 2*(float)h / (float)w, -h, h);
-	else
-		glOrtho(-2, 2 * (float)w / (float)h, -2, -2 * (float)w / (float)h, -w, w);
+	glOrtho(-5 * (float)w / (float)h, 5 * (float)w / (float)h, -5,5, -w, w);
 	glMatrixMode(GL_MODELVIEW);
-	//glutPostRedisplay();
+	glLoadIdentity();
+	glutPostRedisplay();
 }
-
-int main(int argc, char** argv) {
+void idle() {
+	if (alpha >= 360) {
+		alpha -= 360;
+	}
+	alpha += 0.2;
+	glutPostRedisplay();
+}
+void main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
-	glutCreateWindow("CUBE");
-	glutDisplayFunc(cubedisplay);
-	glutIdleFunc(idlecube);
-	glutMouseFunc(cubemouse);
-	glOrtho(0, 300, 0, 300, -300, 300);
-	glutReshapeFunc(cubereshape);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Color Cube");
+	glutDisplayFunc(display);
+	glOrtho(-5, 5, -5, 5, -5, 5);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutIdleFunc(idle);
+
 	glEnable(GL_DEPTH_TEST);
 	glutMainLoop();
-	return 0;
 }
